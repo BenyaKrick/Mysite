adc9f1db3ddc3b3c46b844b93daac0e6862df65a
benyakrck <sergrb27@yandex.ru>
2022-06-14 21:38:47 +0300

    создали вторую страницу

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
 from flask import Flask
 
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-    return "Hello World"
+app = Flask(__name__) #создаем объект
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+@app.route('/') #декоратор для функции рут прописываем url для отслеживания
+def index(): #функция отсеживания главной странички
+    return "Hello World" #отображение на главной страничке
+
+@app.route('/about')
+def about(): #функция отсеживания страницы about
+    return "About page" #отображение на странице about
+
+
+
+
+if __name__ == "__main__": #определяем главный файл для запуска
+    app.run(debug=True) #показываем ошибки на сайте
